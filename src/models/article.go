a633ee8afc51e7539508fd353bb370cb93ac1bf6
Myouuu <meriem.mrabent@hetic.net>
2020-07-22 15:20:44 +0200

    model_article

--- src/models/article.go
+++ src/models/article.go
@@ -1,1 +1,32 @@
 package models
+<<<<<<< Updated upstream
+=======
+
+import (
+"database/sql"
+
+"github.com/gin-gonic/gin"
+)
+// article wiki
+type article struct {
+	article_id      int
+	title           string
+	content         string
+}
+
+func CreateArticle(title string, db *sql.DB) (article, error) {
+	sqlStatement := `
+	INSERT INTO article (title)
+	VALUES ($1) RETURNING article_id, title;`
+
+	var newArticle article
+
+	row := db.QueryRow(sqlStatement, title)
+	err := row.Scan(&newArticle.article_id, &newArticle.title)
+
+	if err != nil {
+		return newArticle, err
+	}
+	return newArticle, nil
+}
+>>>>>>> Stashed changes
