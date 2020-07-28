8d01496ee7e33d43b6738cc1e9239bc7277045d2
Myouuu <meriem.mrabent@hetic.net>
2020-07-28 17:34:17 +0200

    delete interface example

--- Utiltaires/Export.go
+++ Utiltaires/Export.go
@@ -1,12 +1,2 @@
 package Utiltaires
 //
-messages du package message.go
-// MessagePlatform - Comportement pour chaque plateforme
-interface de type MessagePlatform {
-SendMessage (chaîne de message)
-}
-// MessagePlatforms - Le registre des plateformes disponibles
-var MessagePlatforms = map [chaîne] MessagePlatform {
-	"A": PlatformA {Name: "Platform A"},
-	"B": PlatformB {Name: "Platform B"},
-}
