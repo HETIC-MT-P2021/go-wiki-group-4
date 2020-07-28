e326f1aaac73e213eab566e9516c9cfda31ada39
TheoSabot <theo@virutalbox.com>
2020-07-28 17:29:11 +0200

    Added csv creation for article list

---
+++ Utiltaires/csv.go
@@ -0,0 +1,38 @@
+package Utiltaires
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+
+	"packages.hetic.net/gomvc/models"
+)
+
+var articleHeaders = []string{"ArticleID", "Title", "Content"}
+
+// CreateCSVFile create a csv file and save it
+func CreateCSVFile(articles []models.Article) error {
+	file, err := os.Create("aricles.csv")
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	err = writer.Write(articleHeaders)
+
+	for i := 0; i < len(articles); i++ {
+		thisArticleRow := []string{strconv.Itoa(articles[i].ArticleID), articles[i].Title, articles[i].Content}
+		err = writer.Write(thisArticleRow)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
