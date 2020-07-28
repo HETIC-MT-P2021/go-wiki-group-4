4d188c527a6d793951c9b26be838afa7a358ad72
Jean-Jacques <akakpo.jeanjacques@gmail.com>
2020-07-28 12:10:40 +0200

    🗃 Update database structure and add seed files

--- db/db.sql
+++ db/db.sql
@@ -1,15 +1,20 @@
-CREATE DATABASE gomvc;
+CREATE DATABASE IF NOT EXISTS gomvc;
 
-CREATE TABLE article
+USE gomvc;
+
+CREATE TABLE IF NOT EXISTS article
 (
-  article_id serial PRIMARY KEY,
+  article_id bigint(20) AUTO_INCREMENT,
   title VARCHAR (128) NOT NULL,
   content TEXT NOT NULL,
-  UNIQUE (title)
+
+  PRIMARY KEY (article_id)
 );
 
-CREATE TABLE comment (
-    comment_id serial PRIMARY KEY,
-    article_id int references article(article_id),
-    content VARCHAR (256) NOT NULL
+CREATE TABLE IF NOT EXISTS comment (
+  comment_id bigint(20) AUTO_INCREMENT PRIMARY KEY,
+  article_id bigint(20) NOT NULL,
+  content VARCHAR (256) NOT NULL,
+
+  FOREIGN KEY (article_id) REFERENCES article(article_id)
 );
