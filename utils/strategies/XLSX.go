1cfad8aeea1e690310ee71677a65b7f30c955010
Myouuu <meriem.mrabent@hetic.net>
2020-07-29 15:13:23 +0200

    Update XLSX.go

--- utils/strategies/XLSX.go
+++ utils/strategies/XLSX.go
@@ -19,22 +19,21 @@ func InitXLSXStrategy() ExportStrategyInterface {
 	return XLSXStrategy
 }
 
-// ExportArticleXLSX create a xlsx file and send it
+//  create a xlsx file and send it
 func ExportArticleXLSX(datas []models.Article) (ExportedContent, error) {
 	f := excelize.NewFile()
 	// Set value of a cell.
-	f.SetCellValue("Sheet1", "A1", "Title")
-	f.SetCellValue("Sheet1", "B1", "Content")
+	f.SetCellValue("Sheet", "A1", "Title")
+	f.SetCellValue("Sheet", "B1", "Content")
 
 	for index, item := range datas {
-		// first index = 0 and does not exit in excel
-		// second index = 1 is already use before
+
 		index_excel := strconv.Itoa(index + 2)
-		f.SetCellValue("Sheet1", "A"+index_excel, item.Title)
-		f.SetCellValue("Sheet1", "B"+index_excel, item.Content)
+		f.SetCellValue("Sheet", "A"+index_excel, item.Title)
+		f.SetCellValue("Sheet", "B"+index_excel, item.Content)
 	}
 
-	// Save xlsx file by the given path.
+	// Save xlsx file
 	f.SaveAs("article.xlsx")
 
 	data, err := ioutil.ReadFile("article.xlsx")
