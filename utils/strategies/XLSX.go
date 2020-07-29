406aaf6e991ca752564c1b45dd0a678590d36731
Myouuu <meriem.mrabent@hetic.net>
2020-07-29 14:54:06 +0200

    xlsx is probably working

--- utils/strategies/XLSX.go
+++ utils/strategies/XLSX.go
@@ -3,8 +3,10 @@ package utils
 import (
 	"fmt"
 	"github.com/360EntSecGroup-Skylar/excelize"
+	"packages.hetic.net/gomvc/models"
+	"strconv"
 )
-
+/* Example
 func utils() {
 	f := excelize.NewFile()
 	// Create a new sheet.
@@ -18,4 +20,28 @@ func utils() {
 	if err := f.SaveAs("Book1.xlsx"); err != nil {
 		fmt.Println(err)
 	}
+}*/
+type ExportXLSX struct {}
+
+func (exportXLSX ExportXLSX) Export(datas []model.Article) (error) {
+	f := excelize.NewFile()
+	// Set value of a cell.
+	f.SetCellValue("Sheet1", "A1", "Title")
+	f.SetCellValue("Sheet1", "B1", "Content")
+
+	for index, item := range datas {
+		// first index = 0 and does not exit in excel
+		// second index = 1 is already use before
+		index_excel := strconv.Itoa(index + 2)
+		f.SetCellValue("Sheet1", "A" + index_excel, item.Title)
+		f.SetCellValue("Sheet1", "B" + index_excel, item.Content)
+	}
+
+	// Save xlsx file by the given path.
+	if err := f.SaveAs("assets/xlsx/articles_" + formatCurrentDate() + ".xlsx"); err != nil {
+		println(err.Error())
+		return err
+	}
+
+	return nil
 }
