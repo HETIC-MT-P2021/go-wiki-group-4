1a47613c8309ea1e475296a07e83902fb0e6279d
TheoSabot <theo@virutalbox.com>
2020-07-31 15:13:39 +0200

    Added sleep for db con and fixed dockercompose

--- db/db.sql
+++ db/db.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS ARTICLE (
+CREATE TABLE IF NOT EXISTS article (
   article_id serial PRIMARY KEY,
   title VARCHAR (128) NOT NULL,
   content TEXT NOT NULL,
   UNIQUE (title)
 );
 
-CREATE TABLE IF NOT EXISTS COMMENT (
+CREATE TABLE IF NOT EXISTS comment (
   comment_id serial PRIMARY KEY,
   article_id int references article(article_id),
   content VARCHAR (256) NOT NULL
